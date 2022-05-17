b0ca9284041632a79dd3b90ac552da0f9ccb994d
Trần Minh Dũng <tranminhdungggg@gmail.com>
2022-05-17 12:53:42 +0700

    Day 4: input submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,22 +22,6 @@ export class AppComponent {
     age: 31,
     gender: 0,
     avatar: 'https://picsum.photos/200/300',
-    status: 1
-  },
-  {
-    id: 3,
-    name: "Le",
-    age: 21,
-    gender: 1,
-    avatar: 'https://picsum.photos/200/300',
-    status: 0
-  },
-  {
-    id: 4,
-    name: "Dinh",
-    age: 40,
-    gender: 0,
-    avatar: 'https://picsum.photos/200/300',
     status: 0
   }
   ]
@@ -61,6 +45,51 @@ export class AppComponent {
   inputValue = '';
   changeInput(e: any) {
     this.inputValue = e.target.value;
+  }
+
+
+
+  //?
+  inputValues = {
+    // id: "",
+    name: '',
+    age: '',
+    gender: '0',
+    avatar: '',
+    status: '0'
+  };
+
+  //? form đăng kí lấy giá trị từ input
+  // onInputName(event: any, info: string) {
+  //   console.log(event.target.value, info);
+  // }
+  // onInputAge(event: any, info: string) {
+  //   console.log(event.target.value, info);
+  // }
+  onInput(event: any, key: 'name' | 'age' | 'avatar' | 'gender' | 'status') {
+    //! key: 'name' | 'age' chỉ được nhận giá trị của inputValues có sẵn khi định nghĩa
+    this.inputValues[key] = event.target.value;
+  }
+
+  //? Event submit form
+  onSubmit() {
+    //? Thêm data vào form vào mảng teachers
+    this.teachers.push({
+      ...this.inputValues,
+      age: +this.inputValues.age,  //? "+"" đưa age từ chuỗi về số
+      gender: +this.inputValues.gender,
+      status: +this.inputValues.status,
+      id: this.teachers.length + 1
+    });
+    //? Cập nhật lại giá trị cho thẻ input trong form
+    this.inputValues = {
+      name: '',
+      age: '',
+      avatar: '',
+      gender: '0',
+      status: '0'
+    }
+
   }
 }
 
